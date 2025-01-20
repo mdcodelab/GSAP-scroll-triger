4abfae772896fe25f34f5a5f6faecf4bdbc204db
miha11-hash <d_mihaela@msn.com>
2025-01-20 23:59:56 +0200

    scroll text title

---
+++ app/(components)/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className="content-container">
+      <h1>Content container</h1>
+    </div>
+  )
+}
+
+export default Content
